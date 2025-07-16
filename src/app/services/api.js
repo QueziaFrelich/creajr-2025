cdf991d3c0723b5f4355a6f0dd50d09afbd60522
Cauê Chaves Vilela Braga <cauechavesvb@gmail.com>
2025-07-16 14:44:23 -0300

    add env file and vars

--- src/app/services/api.js
+++ src/app/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://back.linquei.com/v1',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 })
 
 api.interceptors.request.use((config) => {
